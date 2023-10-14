83217308240e2725c54e96b6b6083ef4f8271425
Liam <141928126+mouldyWilliam@users.noreply.github.com>
2023-10-14 16:21:20 +0100

    Added initial logic for add/remove buttons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const searchButton = document.getElementById("search-button");
 const moviesContainer = document.getElementById("movies-container");
 
 searchButton.addEventListener("click", function () {
+    moviesContainer.innerHTML = "";
     fetch(`https://www.omdbapi.com/?apikey=50bcaecd&s=${searchInput.value}`)
         .then(res => res.json())
         .then(data => {
@@ -19,7 +20,6 @@ function filterMovieIds(movies) {
 }
 
 function renderSearchedMovies(moviesImdbIdArray) {
-    let moviesHtml = "";
     for (let movieImdbId of moviesImdbIdArray) {
         fetch(`https://www.omdbapi.com/?apikey=50bcaecd&i=${movieImdbId}`)
             .then(res => res.json())
@@ -31,12 +31,31 @@ function renderSearchedMovies(moviesImdbIdArray) {
                     </div>
                     <div id="movie-details-container">
                         <h3>${data.Title}</h3>
-                        <button>Add</button>
+                        <button id="${data.imdbID}" data-imdbid="${data.imdbID}">Add</button>
                         <p3>${data.Plot}</p3>
                     </div>
                 </div>
            
-            `;
+            `
             })
     }
 }
+
+let moviesArray = [];
+
+document.addEventListener("click", function(e){
+    
+    if (e.target.dataset.imdbid && document.getElementById(e.target.dataset.imdbid).textContent == "Add") {
+        moviesArray.push(e.target.dataset.imdbid);
+        document.getElementById(e.target.dataset.imdbid).textContent = "Remove";
+        console.log(moviesArray);
+    }
+    else if (e.target.dataset.imdbid && document.getElementById(e.target.dataset.imdbid).textContent == "Remove") {
+        let filteredArray = moviesArray.filter(function(movie){
+            return movie != e.target.dataset.imdbid;
+        })
+        moviesArray = filteredArray;
+        document.getElementById(e.target.dataset.imdbid).textContent = "Add";
+        console.log(moviesArray);
+    }
+})
